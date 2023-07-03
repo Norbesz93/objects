78a1eec11af2d0e2b622aca0a64cb445df1e32b2
benstonemountain <kohegyibence@yahoo.com>
2023-07-03 18:16:17 +0200

    add changes

--- src/components/CheckList.jsx
+++ src/components/CheckList.jsx
@@ -1,13 +1,44 @@
 import { persons } from "../Util/data";
+import { useState } from "react";
 
 const CheckList = () => {
+
+    const [person, setPerson] = useState(persons);
+    const [reading, setReading] = useState(false);
+
+    const personsWhoLikeReading = () => {
+
+        if (!reading) {
+           setPerson(persons);
+        } else {
+
+        const readers = persons.map((item) => {
+
+        return item.hobbie.includes("Reading") ? item : "";
+
+        });
+                setPerson(readers);
+               
+    }
+    setReading(!reading);
+    }
+
+    
+     
     return (
         <div>
             <h1>CheckList</h1>
-            {persons.map((person) => {
+            <div className="checking">
+            <label htmlFor="">Check </label>
+            <input type="checkbox" checked={reading} onChange={()=>{
+               personsWhoLikeReading();
+            }
+            }/>
+            </div>
+            {person.map((human) => {
                 return (
-                    <div key={person.id}>
-                        <p>{person.firstname} {person.lastname}</p>
+                    <div key={human.id}>
+                        <p>{human.firstname} {human.lastname}</p>
                     </div>
                 )
                 })}
